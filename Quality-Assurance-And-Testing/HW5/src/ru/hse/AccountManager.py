7296d94577d6c4cc9ff937d603fb0646e04d08aa
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2024-03-05 20:41:10 +0300

    Complete getBalance

--- Quality-Assurance-And-Testing/HW5/src/ru/hse/AccountManager.py
+++ Quality-Assurance-And-Testing/HW5/src/ru/hse/AccountManager.py
@@ -104,4 +104,6 @@ class AccountManager:
                 r = resp.response
                 if r is not None and (isinstance(r, float)):
                     return AMR(AMR.SUCCEED, r)
+            case ServerResponse.UNDEFINED_ERROR:
+                return AccountManagerResponse.UNDEFINED_ERROR_RESPONSE
         return AMR(AMR.INCORRECT_RESPONSE, resp)
