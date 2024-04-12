f3d71d9275a67842aad9f5913ae04a5e875757c9
TTPO100AJIEX <69868712+TTPO100AJIEX@users.noreply.github.com>
2024-04-12 16:13:50 +0300

    Testing-HW6 (#19)
    * Get task
    
    * Complete basic version
    
    * Test extrernal - torch

---
+++ Quality-Assurance-And-Testing/HW6/run.sh
@@ -0,0 +1,9 @@
+mkdir -p history
+rm -rf history/$1
+mkdir -p history/$1
+cp calc.py history/$1/calc.py
+pytest --hypothesis-show-statistics ./fuzz.py > history/$1/output.txt
+rm -rf `find . -type d -name __pycache__`
+rm -rf .pytest_cache
+rm -rf .hypothesis
+pip3 freeze > requirements.txt
