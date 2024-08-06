d7251465fa4942aa2c502336047e4ce12b0c984d
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2024-08-06 23:58:20 +0300

    Better SDA

--- CourseProject/SDA/SDA.py
+++ CourseProject/SDA/SDA.py
@@ -1,3 +1,4 @@
+import enum
 import typing
 import joblib
 import itertools
@@ -13,36 +14,58 @@ import tqdm.contrib.itertools
 from .clustquality import apply_cluster_method, cluster_metrics_noground, calc_stage_metr_noground
 from .stageprocess import form_stages, form_stage_bands, form_edges_all, calc_stage_distances, merge_stages_1st_step, merge_stages_2nd_step
 
+class StageMerging(enum.Enum):
+    NONE = 0
+    FIRST = 1
+    SECOND = 2
+    BOTH = 3
+
 class SDA:
     def __init__(
         self,
         n_jobs: int = -1,
         scale: bool = False,
         verbose: bool = True,
+        random_state: int = 0,
 
+        # stage 1
         n_clusters_min: int = 2, n_clusters_max: int = 20, n_clusters: typing.Optional[int] = None,
         k_neighbours_min: int = 20, k_neighbours_max: int = 50, k_neighbours: typing.Optional[int] = None,
-        len_st_thr: typing.List[int] = [0, 20, 40, 60],
-        dist_rate: float = 0.3,
+        
+        # stage 1 - merging
+        st1_merging: StageMerging = StageMerging.BOTH,
+        st1_len_thresholds: typing.List[int] = [0, 20, 40, 60],
+        st1_dist_rate: float = 0.3,
 
-        random_state: int = 0,
+        # stage 2
         n_cl_max_thr: typing.List[int] = [10, 15, 20],
         k_neighb_max_thr: typing.List[int] = [35, 40, 45, 50],
-        n_edge_clusters_min: int = 2, n_edge_clusters_max: int = 15, n_edge_clusters: typing.Optional[int] = None
+        n_edge_clusters_min: int = 2, n_edge_clusters_max: int = 15, n_edge_clusters: typing.Optional[int] = None,
+        
+        # stage 2 - merging
+        st2_merging: StageMerging = StageMerging.BOTH,
+        st2_len_thresholds: typing.List[int] = [0, 20, 40, 60],
+        st2_dist_rate: float = 0.2
     ):
         self.scale = scale
         self.n_jobs = n_jobs
         self.verbose = verbose
+        self.random_state = random_state
 
         self.n_clusters = n_clusters or range(n_clusters_min, n_clusters_max + 1)
         self.k_neighbours = k_neighbours or range(k_neighbours_min, k_neighbours_max + 1)
-        self.len_st_thr = len_st_thr
-        self.dist_rate = dist_rate
         
-        self.random_state = random_state
+        self.st1_merging = st1_merging
+        self.st1_len_thresholds = st1_len_thresholds
+        self.st1_dist_rate = st1_dist_rate
+        
         self.n_cl_max_thr = n_cl_max_thr
         self.k_neighb_max_thr = k_neighb_max_thr
         self.n_edge_clusters = n_edge_clusters or range(n_edge_clusters_min, n_edge_clusters_max + 1)
+        
+        self.st2_merging = st2_merging
+        self.st2_len_thresholds = st2_len_thresholds
+        self.st2_dist_rate = st2_dist_rate
 
     def apply(self, features: numpy.ndarray, df_st_edges: typing.Optional[pandas.DataFrame] = None):
         if self.scale: features = sklearn.preprocessing.StandardScaler().fit_transform(features)
@@ -50,6 +73,37 @@ class SDA:
         if df_st_edges is None: df_st_edges = self.stage1(features)
         result = self.stage2(features, df_st_edges)
         return result, df_st_edges
+
+
+    def merge_stages_iter(
+        self,
+        features: numpy.ndarray,
+        st_edges: numpy.ndarray,
+        merging: StageMerging,
+        len_threshold: int,
+        dist_rate: float
+    ) -> numpy.ndarray:
+        if merging in [ StageMerging.FIRST, StageMerging.BOTH ]:
+            st_edges = merge_stages_1st_step(features, st_edges, len_threshold)
+        if merging in [ StageMerging.SECOND, StageMerging.BOTH ]:
+            st_edges = merge_stages_2nd_step(features, st_edges, dist_rate)
+        return st_edges
+    
+    def merge_stages(
+        self,
+        features: numpy.ndarray,
+        st_edges: numpy.ndarray,
+        merging: StageMerging,
+        len_thresholds: typing.List[int],
+        dist_rate: float
+    ) -> typing.List[typing.Tuple[typing.Optional[int], numpy.ndarray]]:
+        if merging == StageMerging.NONE:
+            return [(None, st_edges)]
+        return [
+            (len_min, self.merge_stages_iter(features, st_edges, merging, len_min, dist_rate))
+            for len_min in len_thresholds
+        ]
+
 
     def stage1_iter(self, features: numpy.ndarray, n_clusters: int, k_neighbours: int) -> pandas.DataFrame:
         n_samples, _ = features.shape
@@ -64,12 +118,10 @@ class SDA:
         st_edges = form_stages(labels) # Forming stages from clusters
 
         # Merging stages
-        df_st_edges = [ ]
-        for len_min in self.len_st_thr:
-            st_edges = merge_stages_1st_step(features, st_edges, len_min)
-            st_edges = merge_stages_2nd_step(features, st_edges, self.dist_rate)
-            df_st_edges.append({ **report, 'Len_min': len_min, 'St_edges': st_edges })
-        return df_st_edges
+        return [
+            { **report, 'Len_min': len_min, 'St_edges': st_edges }
+            for len_min, st_edges in self.merge_stages(features, st_edges, self.st1_merging, self.st1_len_thresholds, self.st1_dist_rate)
+        ]
 
     def stage1(self, features: numpy.ndarray) -> pandas.DataFrame:
         if self.verbose:
@@ -84,6 +136,7 @@ class SDA:
         )
         return pandas.DataFrame(list(itertools.chain(*df_st_edges)))
     
+
     def stage2_iter(
         self,
         features: numpy.ndarray,
@@ -113,26 +166,28 @@ class SDA:
 
         result = [ ]
         for (st_centers, cl_center_type) in zip((st_medians, st_modes, st_means), ('Median', 'Mode', 'Mean')):
-            st_edges_centers = [0] + sorted(st_centers) + [n_samples]
-            report = { **part_report, 'Cl_cen': cl_center_type, 'St_edges': st_edges_centers }
-            _, new_labels = form_stage_bands(st_edges_centers)
+            st_edges_centers = numpy.array([0] + sorted(st_centers) + [n_samples])
+            
+            for len_min, st_edges in self.merge_stages(features, st_edges_centers, self.st2_merging, self.st2_len_thresholds, self.st2_dist_rate):
+                st_dist_ward, st_dist_centr = numpy.mean(calc_stage_distances(features, st_edges), axis = 1)
+                overall_metrics = cluster_metrics_noground(features, form_stage_bands(st_edges)[1])
+                    
+                # Clustering metrics for pairs of adjacent stages
+                df_avg_metrics = calc_stage_metr_noground(features, st_edges).mean()
+                avg_metrics = df_avg_metrics.rename(lambda col: f'Avg-{col}').to_dict()
 
-            st_dist_ward, st_dist_centr = numpy.mean(calc_stage_distances(features, st_edges_centers), axis = 1)
-            overall_metrics = cluster_metrics_noground(features, new_labels)
-                
-            # Clustering metrics for pairs of adjacent stages
-            df_avg_metrics = calc_stage_metr_noground(features, st_edges_centers).mean()
-            avg_metrics = df_avg_metrics.rename(lambda col: f'Avg-{col}').to_dict()
-
-            result.append({ **report, 'Ward_dist': st_dist_ward, 'Cen_dist': st_dist_centr, **overall_metrics, **avg_metrics })
+                result.append({
+                    **part_report, 'Cl_cen': cl_center_type, 'St_edges': st_edges, 'Len_min': len_min,
+                    'Ward_dist': st_dist_ward, 'Cen_dist': st_dist_centr, **overall_metrics, **avg_metrics
+                })
         return result
 
     def stage2(self, features: numpy.ndarray, df_st_edges: pandas.DataFrame) -> pandas.DataFrame:
         if self.verbose:
             print('Running stage 2')
-            loop_over = tqdm.contrib.itertools.product(self.len_st_thr, self.k_neighb_max_thr, self.n_cl_max_thr, self.n_edge_clusters)
+            loop_over = tqdm.contrib.itertools.product(self.st1_len_thresholds, self.k_neighb_max_thr, self.n_cl_max_thr, self.n_edge_clusters)
         else:
-            loop_over = itertools.product(self.len_st_thr, self.k_neighb_max_thr, self.n_cl_max_thr, self.n_edge_clusters)
+            loop_over = itertools.product(self.st1_len_thresholds, self.k_neighb_max_thr, self.n_cl_max_thr, self.n_edge_clusters)
             
         result = joblib.Parallel(n_jobs = self.n_jobs)(
             joblib.delayed(self.stage2_iter)(features, df_st_edges, *params)
--- CourseProject/SDA/__init__.py
+++ CourseProject/SDA/__init__.py
@@ -1,1 +1,2 @@
 from .SDA import SDA
+from .SDA import StageMerging
