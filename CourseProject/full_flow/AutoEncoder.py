ff4333ef0840fd16b16feb3d88dbba93ab79215b
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2024-09-21 17:11:36 +0300

    feature selection

--- CourseProject/full_flow/AutoEncoder.py
+++ CourseProject/full_flow/AutoEncoder.py
@@ -1,21 +1,19 @@
-
 import os
 import random
 
 import tqdm
 import torch
 import numpy
-import pandas
+import matplotlib.pyplot as plt
+import torch.utils.data
 
-subj = "Subj1"
-exp = "exp_full_flow"
-TRY_NUM_FEATURES = numpy.cumsum(numpy.array(range(0, 75, 3))) + 10
 
 device = torch.device(
     "cuda" if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available()
     else "cpu"
 )
+
 
 def set_random_seed(seed: int):
     random.seed(seed)
@@ -24,34 +22,44 @@ def set_random_seed(seed: int):
     torch.cuda.manual_seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
     torch.backends.cudnn.deterministic = True
-    
-class AutoEncoder:
+
+
+class BaseAutoEncoder:
     def __init__(
         self,
         n_features: int,
         n_components: int,
 
-        learning_rate: float = 1e-4,
-        n_epochs: int = 300,
+        batch_size: int = 64,
+        learning_rate: float = 1e-3,
+        n_epochs: int = 250,
         random_state: int = 42
     ):
+        set_random_seed(random_state)
+
         self.encoder = torch.nn.Sequential(
-            torch.nn.Linear(in_features = n_features, out_features = 4000), torch.nn.BatchNorm1d(4000), torch.nn.GELU(),
-            torch.nn.Linear(in_features = 4000, out_features = 1000), torch.nn.BatchNorm1d(1000), torch.nn.GELU(),
+            torch.nn.Linear(in_features = n_features, out_features = 4000), torch.nn.BatchNorm1d(4000), torch.nn.ReLU(),
+            torch.nn.Linear(in_features = 4000, out_features = 1000), torch.nn.BatchNorm1d(1000), torch.nn.ReLU(),
             torch.nn.Linear(in_features = 1000, out_features = n_components)
         ).to(device)
         self.decoder = torch.nn.Linear(in_features = n_components, out_features = n_features).to(device)
         
         self.n_components = n_components
+        self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
         self.random_state = random_state
+    
+    def loss(self, data: torch.Tensor, encoded: torch.Tensor, decoded: torch.Tensor):
+        raise NotImplementedError
     
     def fit(self, data: torch.Tensor):
         def lr_scheduler(epoch: int):
             if epoch < self.n_epochs * 1 // 3: return 1
             if epoch < self.n_epochs * 2 // 3: return 0.1
             return 0.01
+        
+        dataloader = torch.utils.data.DataLoader(data, batch_size = self.batch_size, shuffle = True)
 
         optimizer = torch.optim.AdamW(
             list(self.encoder.parameters()) + list(self.decoder.parameters()),
@@ -59,20 +67,19 @@ class AutoEncoder:
         )
         scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_scheduler)
         
-        loss_history = [ ]
         self.encoder.train()
         self.decoder.train()
         for _ in tqdm.trange(self.n_epochs, desc = f"{self.n_components}"):
-            optimizer.zero_grad()
-            encoded = self.encoder(data)
-            decoded = self.decoder(encoded)
+            for data in dataloader:
+                optimizer.zero_grad()
+                encoded = self.encoder(data)
+                decoded = self.decoder(encoded)
 
-            loss = torch.nn.functional.mse_loss(decoded, data, reduction = 'sum')
-            loss.backward()
-            optimizer.step()
-
-            loss_history.append(loss.item())
-            scheduler.step()
+                loss = self.loss(data, encoded, decoded)
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+        
     
     def fit_transform(self, features: numpy.ndarray):
         set_random_seed(self.random_state)
@@ -80,14 +87,7 @@ class AutoEncoder:
         self.fit(data)
         self.encoder.eval()
         return self.encoder(data).detach().cpu().numpy()
-    
-features = pandas.read_feather(f"{subj}/{exp}/features/features.feather").to_numpy()
-for n_components in TRY_NUM_FEATURES:
-    folder = f"{subj}/{exp}/features_reduced/ae/{n_components}"
-    os.makedirs(folder, exist_ok = True)
 
-    if os.path.exists(f"{folder}/features.npy"):
-        continue
-
-    method = AutoEncoder(n_features = features.shape[1], n_components = n_components)
-    numpy.save(f"{folder}/features.npy", method.fit_transform(features))
+class AutoEncoder(BaseAutoEncoder):
+    def loss(self, data: torch.Tensor, encoded: torch.Tensor, decoded: torch.Tensor):
+        return torch.nn.functional.mse_loss(decoded, data, reduction = 'mean')
--- CourseProject/full_flow/rtd_ae.py
+++ CourseProject/full_flow/rtd_ae.py
@@ -3,47 +3,32 @@ exp = "exp_full_flow"
 
 import os
 
-import numpy
 import torch
-import torch.utils.data
-import pytorch_lightning
+import numpy
 
-from RTD_AE.src.utils import *
 from RTD_AE.src.rtd import RTDLoss
-from RTD_AE.src.autoencoder import AutoEncoder
 
+from AutoEncoder import BaseAutoEncoder
+
+class RTDAutoEncoder(BaseAutoEncoder):
+    def dist(self, data: torch.Tensor):
+        return torch.cdist(data, data) / numpy.sqrt(data.shape[1])
+
+    def loss(self, data: torch.Tensor, encoded: torch.Tensor, decoded: torch.Tensor):
+        if not hasattr(self, 'num_iter'):
+            self.num_iter = 1
+        
+        loss = torch.nn.functional.mse_loss(decoded, data, reduction = 'mean')
+        if self.num_iter % 5 == 0:
+            rtd_loss = RTDLoss(n_threads = -1, engine = "ripser")
+            loss_xz, loss_zx, rtd = rtd_loss(self.dist(data), self.dist(encoded))
+            loss += rtd
+            
+        self.num_iter += 1
+        return loss
+
+features = numpy.load(f"{subj}/{exp}/qsda/best_features.npy")
 TRY_NUM_FEATURES = numpy.cumsum(numpy.array(range(0, 75, 3))) + 10
-
-config = {
-    "max_epochs": 100,
-    "rtd_every_n_batches": 1,
-    "rtd_start_epoch": 0,
-    "rtd_l": 1.0, 
-    "card": 50,
-    "n_hidden_layers": 2,
-    "hidden_dim": 1000,
-    "batch_size": 64,
-    "engine": "ripser",
-    "is_sym": True,
-    "lr": 5e-4
-}
-
-def collate_with_matrix(samples):
-    indicies, data, labels = zip(*samples)
-    data, labels = torch.tensor(numpy.asarray(data)), torch.tensor(numpy.asarray(labels))
-    if len(data.shape) > 2:
-        dist_data = torch.flatten(data, start_dim=1)
-    else:
-        dist_data = data
-    x_dist = torch.cdist(dist_data, dist_data, p=2) / numpy.sqrt(dist_data.shape[1])
-    return data, x_dist, labels
-
-features = numpy.load(f"{subj}/{exp}/features/features.npy").astype(numpy.float32)
-config['input_dim'] = features.shape[1]
-
-ds = FromNumpyDataset(features, geodesic = False, scaler = FurthestScaler(), flatten = True, n_neighbors = 2)
-val_loader = torch.utils.data.DataLoader(ds, batch_size = config["batch_size"], num_workers = 16, collate_fn = collate_with_matrix)
-train_loader = torch.utils.data.DataLoader(ds, batch_size = config["batch_size"], num_workers = 16, collate_fn = collate_with_matrix, shuffle = True)
 
 for n_components in TRY_NUM_FEATURES:
     folder = f"{subj}/{exp}/features_reduced/rtd_ae/{n_components}"
@@ -52,18 +37,5 @@ for n_components in TRY_NUM_FEATURES:
     if os.path.exists(f"{folder}/features.npy"):
         continue
     
-    config['latent_dim'] = n_components
-
-    model = AutoEncoder(
-        encoder = get_linear_model(m_type = 'encoder', **config),
-        decoder = get_linear_model(m_type = 'decoder', **config),
-        RTDLoss = RTDLoss(dim = 1, lp = 1.0,  **config),
-        MSELoss = torch.nn.MSELoss(),
-        **config
-    )
-
-    trainer = pytorch_lightning.Trainer(gpus = -1, max_epochs = config['max_epochs'], log_every_n_steps = 1, num_sanity_val_steps = 0)
-    trainer.fit(model, train_loader, val_loader)
-
-    latent, labels = get_latent_representations(model, val_loader)
-    numpy.save(f"{folder}/features.npy", latent)
+    ae = RTDAutoEncoder(n_features = features.shape[1], n_components = n_components)
+    numpy.save(f"{folder}/features.npy", ae.fit_transform(features))
