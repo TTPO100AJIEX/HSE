adb0020d5115e3a8e9980f5465f8ee962313b2a3
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2026-01-17 17:46:33 +0300

    improve

--- Diploma/examples/reproduction.sh
+++ Diploma/examples/reproduction.sh
@@ -1,5 +1,5 @@
 python3.10 -m venv env
 source env/bin/activate
-python3.10 -m pip install cv-tda cv-tda[classification] cv-tda[facerecognition] cv-tda[autoencoder] cv-tda[segmentation]
-python3.10 -m pip install -e git+https://github.com/c-hofer/torchph.git@master#egg=torchph
+python3.10 -m pip install --no-cache-dir cv-tda cv-tda[classification] cv-tda[facerecognition] cv-tda[autoencoder] cv-tda[segmentation]
+python3.10 -m pip install --no-cache-dir -e git+https://github.com/c-hofer/torchph.git@master#egg=torchph
 python3.10 reproduction.py --quick --clean > quick.txt
